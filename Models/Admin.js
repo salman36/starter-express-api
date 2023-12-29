c58e7128fba5783232d7013f37c6b617297b5734
CORP\muhammadsuleman.khan <muhammadsuleman.khan@ibex.co>
2023-12-29 20:56:03 +0500

    jwt expiry set

--- Models/Admin.js
+++ Models/Admin.js
@@ -41,11 +41,8 @@ adminSchema.pre("save", async function (next) {
 
 // JWT TOKEN
 adminSchema.methods.getJWTToken = function () {
-  console.log(`${process.env.JWT_EXPIRE}`);
   return jwt.sign({ id: this._id }, `${process.env.JWT_SECRET}`, {
-    expiresIn: process.env.JWT_EXPIRE,
-    
-  });
+  }, 'secretKey', { expiresIn: '1h' });
 };
 
 //// compare password /////
