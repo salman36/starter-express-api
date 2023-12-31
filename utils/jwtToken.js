2e69306c21cf81e5517e5ad02158e51771488582
CORP\muhammadsuleman.khan <muhammadsuleman.khan@ibex.co>
2023-12-31 22:24:35 +0500

    jwt expiry set

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,11 +3,11 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
   //   option for cookie
+  const cookieExpire = process.env.COOKIE_EXPIRE || 5;
   const options = {
-    expires: new Date(
-      Date.now() + `${process.env.COOKIE_EXPIRE}` * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
+  
+      expires: new Date(Date.now() + cookieExpire * 24 * 60 * 60 * 1000),
+      httpOnly: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
